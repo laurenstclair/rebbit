81010d046721777ae5ae5e1cf294c5ffacc0ccdd
Kathryn Lecha <43145003+kzlecha@users.noreply.github.com>
2020-02-20 08:59:55 -0800

    Update Dash query-exclude knots where user is banned
    I should have built this with Cassandra so each table is specifically created for one query and there are less joins and subqueries.

--- data/sql_queries.sql
+++ data/sql_queries.sql
@@ -18,16 +18,23 @@ FROM Posts AS p, Knot AS k
 WHERE p.knot_id = k.knot_id
     AND knot_id IN(SELECT knot_id
                    FROM FollowingKnot
-                   WHERE user_id = ?);
+                   WHERE user_id = ?)
+    AND knot_id NOT IN(SELECT knot_id
+                       FROM BannedFromKnot
+                       WHERE user_id = ?);
 
 -- Get Comments on a Post
 SELECT comment_id
 FROM comment_id
 WHERE post_id = ?;
 
--- TODO: Comments on comments
 SELECT comment_id
 FROM comment_id
 WHERE post_id = ?
     AND parent_comment_id == ?;
 
+-- Determine if a user is banned from a table
+SELECT *
+FROM BannedFromKnot
+WHERE user_id = ?
+    AND knot_id = ?;
