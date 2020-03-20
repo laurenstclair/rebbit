0427ee5da6654abbb1401a1e5957bc96be9d0086
Lauren St. Clair <lauren.d.stclair@gmail.com>
2020-03-20 10:11:48 -0700

    Add form styling

--- register.php
+++ register.php
@@ -101,7 +101,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     mysqli_close($link);
 }
 ?>
- 
  <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -110,9 +109,48 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="./bootstrap/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    
     <title>Rebbit - Frogs 24/7</title>
 </head>
+<style>
+
+.register{
+    background-color: #4f676c;
+    padding: 20px;
+    overflow: auto;
+    color: #e9f5ef;
+    border-radius: 25px;
+    max-width: 600px;
+}
+
+.submit_btn{
+    background-color: darkseagreen;
+    border-color: #9eb9ab;
+}
+
+.reset_btn{
+    color:darkseagreen;
+}
+
+.site_link{
+    color: darkseagreen;
+}
+
+.submit_btn:hover{
+    background-color: #9eb9ab;
+    border-color: darkseagreen;
+}
+
+.reset_btn:hover{
+    color: #9eb9ab;
+}
+
+.site_link:hover{
+    color: #86e4aa;
+}
+
+
+</style>
+
 <body>
     <body style="margin: 0.25em; background-color: #f1f0f0;">
         <!-- NAVIGATION BAR-->
@@ -149,38 +187,46 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 </form>
             </div>
         </nav>
-    <div class="wrapper">
+
+    <!-- Sign Up Form -->
+    <div class="wrapper register" style="padding-top: 40px; padding: 20px;">
         <h2>Sign Up</h2>
-        <p>Please fill this form to create an account.</p>
+        <p>Please create a username and password to create an account.</p>
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
-                <label>Username</label>
-                <input type="text" name="username" class="form-control" value="<?php echo $user_name; ?>">
-                <span class="help-block"><?php echo $username_err; ?></span>
+            <div class="form-group row<?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+                <div class="col-md-6">
+                    <label>Username</label>
+                    <input type="text" name="username" class="form-control" value="<?php echo $user_name; ?>">
+                    <span class="help-block"><?php echo $username_err; ?></span>
+                </div>
             </div>    
             <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
-                <label>Password</label>
-                <input type="password" name="password" class="form-control" value="<?php echo $password; ?>">
-                <span class="help-block"><?php echo $password_err; ?></span>
+                <div class="col-md-6">
+                    <label>Password</label>
+                    <input type="password" name="password" class="form-control" value="<?php echo $password; ?>">
+                    <span class="help-block"><?php echo $password_err; ?></span>
+                </div>
             </div>
             <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
-                <label>Confirm Password</label>
-                <input type="password" name="confirm_password" class="form-control" value="<?php echo $confirm_password; ?>">
-                <span class="help-block"><?php echo $confirm_password_err; ?></span>
+                <div class="col-md-6">
+                    <label>Confirm Password</label>
+                    <input type="password" name="confirm_password" class="form-control" value="<?php echo $confirm_password; ?>">
+                    <span class="help-block"><?php echo $confirm_password_err; ?></span>
+                </div>
             </div>
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Submit">
-                <input type="reset" class="btn btn-default" value="Reset">
+            <div class="form-group col-md-6">
+                <input type="submit" class="btn btn-primary submit_btn" value="Submit">
+                <input type="reset" class="btn btn-default reset_btn" value="Reset">
             </div>
-            <p>Already have an account? <a href="login.php">Login here</a>.</p>
+            <p>Already have an account? <a class="site_link" href="login.php">Login</a></p>
         </form>
     </div>    
 
-      <!-- Optional JavaScript -->
-        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-            <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-            <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-            <script src="./bootstrap/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="./bootstrap/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
 </body>
 </html>
